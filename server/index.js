66a306574a93c5986024bb17e5744b856c2a1025
Richard Ivan <richardalfredivan@gmail.com>
2016-09-13 13:27:02 +0000

    chore(heroku): test of server path for heroku

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var path = require('path')
 var app = express();
 
 var port = process.env.PORT || 8080;
 
-app.use('/', express.static('dist'));
+console.log(path.join(__dirname, '/dist'))
+app.use('/', express.static(__dirname + '/dist'));
 
 app.listen(port, function() {
     console.log('Our app is running on http://localhost:' + port);
