b0c1bfdacedca016fc353242f84b02d12fe3ca20
Richard Ivan <richardalfredivan@gmail.com>
2016-09-13 13:34:22 +0000

    chore(server): fix relative path in express

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ var app = express();
 var port = process.env.PORT || 8080;
 
 console.log(path.join(__dirname, '/dist'))
-app.use('/', express.static(__dirname + '/dist'));
+app.use('/', express.static('dist'));
 
 app.listen(port, function() {
     console.log('Our app is running on http://localhost:' + port);
